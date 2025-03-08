411e48aef14225d078fcdfa587fee53440aa17ce
Laurent Ouma <lawiomosh3@gmail.com>
2025-03-08 13:04:11 +0300

    feat: enhance command event handling with logging and new connection broadcasts

--- src/socket/events/command.ts
+++ src/socket/events/command.ts
@@ -1,9 +1,17 @@
+import logger from "@/services/logger";
 import { DefaultEventsMap, Socket } from "socket.io";
 
 const commandEvents = (
   socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
 ) => {
-  
+  socket.broadcast.emit("new_connection", {
+    client: socket.client.conn.remoteAddress,
+    client1: socket.conn.remoteAddress,
+  });
+  socket.on("cmd:send", (cmd) => {
+    logger.info("Message Received: ", cmd);
+    socket.broadcast.emit("cmd:send", cmd);
+  });
 };
 
 export default commandEvents;
